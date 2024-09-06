121eff9374781e9429b717eaa8a783eb53236c11
Harsh Parihar <134674732+harsh-a-parihar@users.noreply.github.com>
2024-09-06 15:45:45 +0530

    components, connect db, update backend, signup.tsx

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const cors = require('cors');
+require("./db/config");
+const User = require('./db/Users');
 const app = express();
 
-// create app route
-app.get("/", (req, resp)=> {
-    resp.send("app is running!!")
-})
 
-// listening of port 3000
-app.listen(3000);
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// create app route
+app.post("/signup", async (req, resp) => {
+    console.log("Request Body:", req.body); // Log to verify request payload
+    try {
+        let user = new User(req.body);
+        let result = await user.save();
+        console.log("User saved:", result);
+        resp.send(result);
+    } catch (error) {
+        console.error("Error saving user:", error);
+        resp.status(500).send({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+app.listen(4000);
